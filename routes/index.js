469dd1575ef3521cb7991f7367ef0fec209b3dee
Felquis Gimenes <frgformenton@gmail.com>
2013-11-25 23:36:30 -0200

    Add basic initial components

--- routes/index.js
+++ routes/index.js
@@ -9,3 +9,15 @@ exports.index = function(req, res){
   	title: 'I\'m Happy For... my family, my work, home'
   });
 };
+
+
+/*
+ * GET test page.
+ */
+
+exports.test = function(req, res){
+  res.render('test', {
+  	imhappyfor: 'I\'m Happy For...',
+  	title: 'I\'m Happy For... my family, my work, home'
+  });
+};
